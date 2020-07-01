aa97b17a4059ff67006487d376e013fcf92aa795
kunalkumar1989 <kunalkumar1989@users.noreply.github.com>
2020-07-01 15:18:33 +0530

    Challenge 1
    on click of image or name it will open details
    
    On click of location it will open  all details of charectors in modal

---
+++ task-1/js/custom-home-js.js
@@ -0,0 +1,40 @@
+$(document).on('click','.location-link',function() {
+	var url = $(this).attr('data-href');
+	$('.my-new-list').remove();
+	$.getJSON(url, function( data ) {
+		var items = [];
+		$.each( data, function( key, val ) {
+			if(key == 'residents'){
+				$.each( val, function( key1, val1 ) {
+					$.getJSON( val1, function( data1 ) {
+						$.each( data1, function( key2, val2 ) {
+							if(key2 == 'name')
+							items.push( "<li class='is-size-5 has-text-centered name has-text-danger' id='" + key2 + "'><span class='has-text-dark is-capitalized has-text-weight-semibold'>" + key2 +" :</span> "+val2 + "</li>" );
+							if(key2 == 'status')
+							items.push( "<li class='is-size-5 has-text-centered status has-text-primary	' id='" + key2 + "'><span class='has-text-dark is-capitalized has-text-weight-semibold'>" + key2 +" :</span> "+val2 + "</li>" );
+						});
+						$( "<ul/>", {
+							"class": "my-new-list",
+							html: items.join( "" )
+						}).appendTo( ".result" );
+					});
+				});
+			}
+		});
+	});
+	$('.modal').addClass('is-block');
+});
+$(document).on('click','.modal-close, .modal-background',function() {
+	$('.modal').removeClass('is-block');
+});
+
+$(document).on('click','.image-anchor',function() {
+$(this).closest('.column').siblings('.column').find('a.details-info')[0].click();
+});
+
+$(document).on('click','.details-info',function() {
+	var url = $(this).attr('data-href');
+	localStorage.setItem("details-url", url);
+	var imgUrl = $(this).parent().siblings('.column').find('img').attr('src');
+	localStorage.setItem("imgurl", imgUrl);
+});
